55133bde95368656c3246c9bf1a5474cb0c42e68
Paulo Ramos <paulo.ramos.ccr@gmail.com>
2022-08-09 16:32:46 -0300

    Refatoração do JS

--- js/index.js
+++ js/index.js
@@ -1,40 +1,40 @@
 function revelarResposta() {
-    var resposta = document.querySelector("#resposta");
-    resposta.classList.toggle("blur");
+  var resposta = document.querySelector("#resposta");
+  resposta.classList.toggle("blur");
 }
-  
+
 function proximaPergunta(proximaPergunta) {
-    var pergunta = document.querySelector("#cardContainer");
-    pergunta.innerHTML = "";
-  
-    var cardDiv = document.createElement("div");
-  
-    cardDiv.classList.add("card", "animate__animated", "animate__backInRight");
-  
-    console.log(proximaPergunta);
-  
-    cardDiv.innerHTML = `
-      <div class="card-cabecalho centralizar">
-        <h1 class="card-titulo">o que é ${proximaPergunta.title}?</h1>
-      </div>
-  
-      <div id="resposta" class="card-conteudo blur">
-        <p>
-          ${proximaPergunta.description}
-        </p>
-      </div>
-    `;
-    pergunta.appendChild(cardDiv);
+  var pergunta = document.querySelector("#cardContainer");
+  pergunta.innerHTML = "";
+
+  var cardDiv = document.createElement("div");
+  cardDiv.classList.add("card", "animate__animated", "animate__backInRight");
+
+  cardDiv.innerHTML = `
+  <div class="card-cabecalho centralizar">
+
+  <h1 class="card-titulo">o que é ${proximaPergunta.title}?</h1>
+</div>
+
+<div id="resposta" class="card-conteudo blur">
+
+  <p>
+    ${proximaPergunta.description}
+  </p>
+</div>
+`;
+  pergunta.appendChild(cardDiv);
 }
-  
+
 function buscarInformacao() {
-    fetch("https://flash.quickstaart.com/random")
-      .then(function (resultado) {
-        return resultado.json();
-      })
-      .then(function (resultadoJson) {
-        proximaPergunta(resultadoJson);
-      });
+  fetch("https://flash.quickstaart.com/random")
+    .then(function (resultado) {
+      return resultado.json();
+    })
+
+    .then(function (resultadoJson) {
+      proximaPergunta(resultadoJson);
+    });
 }
-  
+
 window.addEventListener("load", buscarInformacao);
